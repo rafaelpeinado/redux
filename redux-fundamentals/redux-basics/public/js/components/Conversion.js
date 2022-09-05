df74e006fa73c7e586652e7d45a3b252aaf6b4d3
rafaelpeinado <rafael.peinado.silva@gmail.com>
2022-09-05 15:35:51 -0300

    wip: finaliza redux building blocks

--- redux-fundamentals/redux-basics/public/js/components/Conversion.js
+++ redux-fundamentals/redux-basics/public/js/components/Conversion.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { createStore } from 'redux';
 import PropTypes from 'prop-types'
 import axios from 'axios';
 import debounce from 'lodash.debounce';
+
+var defaultState = {
+    originAmount: '0.00'  
+};
+
+// variável reducer
+// no ES6 state = defaultState é o mesmo que state = state || defaultState;
+// obs: quando criamos o store, ele chama o dispatch uma vez, por isso estava retornando 2, por isso +
+// foi preciso definir type, para ele incrementar só quando o type INCREMENT for chamado
+function amount(state = defaultState, action) {
+    // state = state || defaultState;
+
+    if (action.type === 'CHANGE_ORIGIN_AMOUNT'){
+        // estamos quebrando uma das principais e mais difíceis regras do redux, que é não podemos
+        // alterar o state. Só podemos fazer atualizações imutáveis, pois quando alteramos o objeto
+        // diretamente, perdemos todo o histórico de atualizações do estado, e o redux usa o histórico
+        // para fazer comparações para ver se o STATE mudou.
+        // state.originAmount = action.data; então:
+
+        // podemos usar outra sintaxe para esse retorno
+        // return Object.assign({}, state, { originAmount: action.data }); então:
+
+        // ... é chamado de Operador de Propagação de Objeto (Object Spread)
+        // ... copia e espelha todo o objeto e ainda atualiza com o novo action.data
+        // ... é equivalente a Object.assign({}, state, { originAmount: action.data });
+        return {
+            ...state,
+            originAmount: action.data
+        }
+    }
+    return state;
+
+    // return action.type === 'CHANGE_ORIGIN_AMOUNT' ? Object.assign({}, state, { originAmount: action.data }) : state;
+}
+
+// com o STORE queremos ou atualizar ou definir o STATE
+var store = createStore(amount);
+
+// podemos usar subscribe nas atualizações do store
+store.subscribe(function() {
+    console.log('state', store.getState());
+});
+
+// REDUCER é um conjunto que é executado toda vez que uma ação é disparado no STORE
+
+// a forma de atualizar um reducer daquele store é usando um dispatch, que dispara uma ação
+// e essa ação dever ser um objeto
+store.dispatch({
+    type: 'CHANGE_ORIGIN_AMOUNT',
+    data: '300.65'
+});
+
+store.dispatch({
+    type: ''
+});
 
 class FeesTable extends React.Component {
     render() {
