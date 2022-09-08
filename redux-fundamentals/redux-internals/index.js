5f8268e8f94bdfadd1a621a623735729cd3af2cf
rafaelpeinado <rafael.peinado.silva@gmail.com>
2022-09-08 11:48:38 -0300

    wip: finaliza writing createStore()

--- redux-fundamentals/redux-internals/index.js
+++ redux-fundamentals/redux-internals/index.js
@@ -21,3 +21,45 @@ console.log('state:after', store.getState());
 
 unsub();
 store.dispatch({type: 'INCREMENT'});
+
+function createStore(reducer) {
+  var state;
+  var subscriptions = [];
+
+  // ao fazer o createStore e em seguida fizer um dispatch, o state ainda estará
+  // undefined. O dispatch precisa ser executado ao menos uma ve para que o redutor
+  // faça o subscribe no state e retorna à função createStore
+  // arrumamos isso chamando o dispatch uma vez na função createStore
+  // return {
+  var obj = {
+    getState: function() {
+      return state;
+    },
+    dispatch: function(action) {
+      // call the reducer
+      // usamos o state = , pois na próxima vez que chamarmos o getState
+      // ele retornará o state mais recente. E se não colocarmos, o state
+      // será sempre undefined
+      state = reducer(state, action);
+      // call the subscribed fns
+      subscriptions.forEach(function(fn) {
+        fn();
+      });
+    },
+    subscribe: function(fn) {
+      // call functions when dispatch is called
+      subscriptions.push(fn);
+      // returns an unsubscribe function
+      return function unsubscribe() {
+        // find listener fn in sub array and remove it
+        var index = subscriptions.indexOf(fn);
+        subscriptions.splice(index, 1);
+      }
+    }
+  };
+
+  obj.dispatch({ type: 'REDUX_INIT' });
+  return obj;
+}
+
+
